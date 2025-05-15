52b6b9d936503a2f71620af725638eaf035eba21
TEST PROJ <73068135+LazyLlama1147@users.noreply.github.com>
2025-05-15 18:15:48 -0400

    Add files via upload

---
+++ build/CMakeFiles/FINAL_334CS.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FINAL_334CS.dir/lib/glad.c.o"
+  "CMakeFiles/FINAL_334CS.dir/lib/glad.c.o.d"
+  "CMakeFiles/FINAL_334CS.dir/src/main.cpp.o"
+  "CMakeFiles/FINAL_334CS.dir/src/main.cpp.o.d"
+  "FINAL_334CS"
+  "FINAL_334CS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/FINAL_334CS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/FINAL_334CS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FINAL_334CS.
+# This may be replaced when dependencies are built.
